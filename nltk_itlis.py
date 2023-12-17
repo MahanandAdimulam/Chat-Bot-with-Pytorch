4d7cff467d27f28e73eb75ddcbaddc669aaa828a
MahanandAdimulam <ma9333@rit.edu>
2023-12-17 15:44:13 -0500

    nltk_utils.py

--- nltk_itlis.py
+++ nltk_itlis.py
@@ -1,6 +1,7 @@
 import nltk
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
@@ -15,3 +16,6 @@ a = "How long does shippin take?"
 print(a)
 a = tokenize(a)
 print(a)
+words = ["organize", "organizes","organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
