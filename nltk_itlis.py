fca4e10ed03dbc37357a01d5636ff89df60e8605
MahanandAdimulam <ma9333@rit.edu>
2023-12-19 14:32:45 -0500

    bag_of_words function

--- nltk_itlis.py
+++ nltk_itlis.py
@@ -1,5 +1,7 @@
 import nltk
+import numpy as np
 # nltk.download('punkt')
+
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -10,7 +12,12 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sen, all_words):
-    pass
+    tokenized_sen = [stem(w) for w in tokenized_sen]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sen:
+            bag[idx] = 1.0
+    return bag
 
 # a = "How long does shippin take?"
 # print(a)
